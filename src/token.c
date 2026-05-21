4aabfd88cdbd1fe4044ddc5cdf756141605370c2
Seaver Olson <sjolson007@gmail.com>
2026-05-21 00:02:07 -0500

    forgot to push helper functions. stu pid vscode git push extension

--- src/token.c
+++ src/token.c
@@ -9,3 +9,40 @@ const char* token_to_string(TokenType type) {
   }
 }
 
+TokenType string_to_token(const char* str) {
+  #define IF(str_literal, tok) if (strcmp(str, str_literal) == 0) return tok;
+  SEA_TOKEN_LIST(IF)
+  #undef IF
+  return TK_UNKNOWN;
+}
+
+bool is_keyword(const char* str) {
+  #define IF(str_literal, tok) if (strcmp(str, str_literal) == 0) return true;
+  SEA_KEYWORD_TOKENS(IF)
+  #undef IF
+  return false;
+}
+
+bool is_operator(const char* str) {
+  #define IF(str_literal, tok) if (strcmp(str, str_literal) == 0) return true;
+  SEA_OPERATOR_TOKENS(IF)
+  #undef IF
+  return false;
+}
+
+bool is_punctuation(const char* str) {
+  #define IF(str_literal, tok) if (strcmp(str, str_literal) == 0) return true;
+  SEA_PUNCTUATION_TOKENS(IF)
+  #undef IF
+  return false;
+}
+
+bool is_literal(TokenType type) {
+  switch (type) {
+    #define CASE(type, str) case type: return true;
+    SEA_LITERAL_TOKENS(CASE)
+    #undef CASE
+    default: return false;
+  }
+}
+
